9dfb555994e3040e5abec691d4625fb8b8dea85c
Zouhair Karib <zouhair.karib.1997@gmail.com>
2023-03-16 19:40:52 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 /* a program to print a string using puts function */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
